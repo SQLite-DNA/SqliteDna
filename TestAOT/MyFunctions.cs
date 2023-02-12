f1914ece5731bf5a86ca3a5b3771478b51314868
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2023-02-12 21:13:42 +0500

    Added TestAOT project. Added support for AOT mode.

---
+++ TestAOT/MyFunctions.cs
@@ -0,0 +1,15 @@
+﻿namespace TestAOT
+{
+    public class MyFunctions
+    {
+        public static int Boo2()
+        {
+            return 2;
+        }
+
+        public static int Boo42()
+        {
+            return 42;
+        }
+    }
+}
