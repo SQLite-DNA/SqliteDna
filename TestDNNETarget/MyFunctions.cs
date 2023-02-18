a20ab23bdedf928e0364b40cbfe62966515c2174
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2023-02-18 14:10:55 +0500

    Created TestDNNETarget project.

---
+++ TestDNNETarget/MyFunctions.cs
@@ -0,0 +1,15 @@
+﻿namespace TestDNNETarget
+{
+    public class MyFunctions
+    {
+        public static int Too2()
+        {
+            return 2;
+        }
+
+        public static int Too42()
+        {
+            return 42;
+        }
+    }
+}
