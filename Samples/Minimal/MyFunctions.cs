eae02f501715d6279eb6405ecbd96da4bcc5c72d
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2023-02-26 17:06:51 +0500

    Added [Function] to Samples.

--- Samples/Minimal/MyFunctions.cs
+++ Samples/Minimal/MyFunctions.cs
@@ -1,12 +1,16 @@
-﻿namespace Minimal
+﻿using SqliteDna.Integration;
+
+namespace Minimal
 {
     public class MyFunctions
     {
+        [Function]
         public static int Foo2()
         {
             return 2;
         }
 
+        [Function]
         public static int Foo42()
         {
             return 42;
--- Samples/MinimalAOT/MyFunctions.cs
+++ Samples/MinimalAOT/MyFunctions.cs
@@ -1,12 +1,16 @@
-﻿namespace MinimalAOT
+﻿using SqliteDna.Integration;
+
+namespace MinimalAOT
 {
     public class MyFunctions
     {
+        [Function]
         public static int Foo2()
         {
             return 2;
         }
 
+        [Function]
         public static int Foo42()
         {
             return 42;
