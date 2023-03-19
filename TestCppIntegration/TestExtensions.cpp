34a985485a25c631626610b3fd0fbe115757bdb1
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2023-03-19 20:11:39 +0500

    Added remaining function tests for C++.

--- TestCppIntegration/TestExtensions.cpp
+++ TestCppIntegration/TestExtensions.cpp
@@ -26,9 +26,69 @@ namespace TestCppIntegration
 			SQLite::Database db(":memory:");
 			db.loadExtension(extensionFile.c_str(), nullptr);
 			{
-				SQLite::Statement   query(db, "SELECT Foo42()");
+				SQLite::Statement query(db, "SELECT Foo2()");
+				query.executeStep();
+				Assert::AreEqual(2, query.getColumn(0).getInt());
+			}
+			{
+				SQLite::Statement query(db, "SELECT Foo42()");
 				query.executeStep();
 				Assert::AreEqual(42, query.getColumn(0).getInt());
+			}
+			{
+				SQLite::Statement query(db, "SELECT FooHello()");
+				query.executeStep();
+				Assert::AreEqual(std::string("Hello"), query.getColumn(0).getString());
+			}
+			{
+				SQLite::Statement query(db, "SELECT MyIntSum(2, 3)");
+				query.executeStep();
+				Assert::AreEqual(5, query.getColumn(0).getInt());
+			}
+			{
+				SQLite::Statement query(db, "SELECT MyInt64Sum(223372036854775807, 2)");
+				query.executeStep();
+				Assert::AreEqual(223372036854775809, query.getColumn(0).getInt64());
+			}
+			{
+				SQLite::Statement query(db, "SELECT MyDoubleSum(1.2, 1.3)");
+				query.executeStep();
+				Assert::AreEqual(2.5, query.getColumn(0).getDouble());
+			}
+			{
+				SQLite::Statement query(db, "SELECT MyConcat('Hello', 'world')");
+				query.executeStep();
+				Assert::AreEqual(std::string("Hello world"), query.getColumn(0).getString());
+			}
+			{
+				Assert::AreEqual(0, db.exec("SELECT Nop()"));
+			}
+			{
+				Assert::AreEqual(0, db.exec("SELECT IncrementInternalCounter()"));
+
+				SQLite::Statement query(db, "SELECT GetInternalCounter()");
+				query.executeStep();
+				Assert::AreEqual(1, query.getColumn(0).getInt());
+			}
+			{
+				SQLite::Statement query1(db, "SELECT MyNullableConcat('Hello', 'world')");
+				query1.executeStep();
+				Assert::AreEqual(std::string("Hello world"), query1.getColumn(0).getString());
+
+				SQLite::Statement query2(db, "SELECT MyNullableConcat(NULL, NULL)");
+				query2.executeStep();
+				Assert::IsTrue(query2.getColumn(0).isNull());
+
+				SQLite::Statement query3(db, "SELECT MyNullableConcat('Hello', NULL)");
+				query3.executeStep();
+				Assert::AreEqual(std::string("Hello"), query3.getColumn(0).getString());
+
+				SQLite::Statement query4(db, "SELECT MyNullableConcat(NULL, 'world')");
+				query4.executeStep();
+				Assert::AreEqual(std::string("world"), query4.getColumn(0).getString());
+			}
+			{
+				Assert::ExpectException<SQLite::Exception>([&]() { db.exec("SELECT Noo1()"); });
 			}
 		}
 	};
--- TestIntergration/TestExtensions.cs
+++ TestIntergration/TestExtensions.cs
@@ -35,6 +35,11 @@ namespace TestIntergration
                 }
                 {
                     var command = connection.CreateCommand();
+                    command.CommandText = @"SELECT MyInt64Sum(223372036854775807, 2)";
+                    Assert.Equal(223372036854775809, (long)command.ExecuteScalar()!);
+                }
+                {
+                    var command = connection.CreateCommand();
                     command.CommandText = @"SELECT MyDoubleSum(1.2, 1.3)";
                     Assert.Equal(2.5, (double)command.ExecuteScalar()!);
                 }
@@ -79,22 +84,6 @@ namespace TestIntergration
                     command.CommandText = @"SELECT Noo1()";
                     Assert.Throws<SqliteException>(command.ExecuteScalar);
                 }
-            }
-        }
-
-        [Theory]
-        [InlineData("TestDNNENE.dll")]
-        [InlineData("TestAOT.dll")]
-        public void Int64Sum(string extensionFile)
-        {
-            using (var connection = new SqliteConnection("Data Source=:memory:"))
-            {
-                connection.Open();
-                connection.LoadExtension(extensionFile);
-
-                var command = connection.CreateCommand();
-                command.CommandText = @"SELECT MyInt64Sum(223372036854775807, 2)";
-                Assert.Equal(223372036854775809, (long)command.ExecuteScalar()!);
             }
         }
 
