8d654f81ce0deea2de533ec2bf8b6b478fb59a35
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2023-07-09 21:05:50 +0500

    Added exception, database, DateTime, blob tests.

--- TestCsIntegration/TestExtensions.cs
+++ TestCsIntegration/TestExtensions.cs
@@ -27,6 +27,72 @@ namespace TestCsIntegration
                 Assert.Equal(DBNull.Value, connection.ExecuteScalar<DBNull>("SELECT MyNullableConcat(NULL, NULL)"));
                 Assert.Equal("Hello", connection.ExecuteScalar<string>("SELECT MyNullableConcat('Hello', NULL)"));
                 Assert.Equal("world", connection.ExecuteScalar<string>("SELECT MyNullableConcat(NULL, 'world')"));
+                Assert.ThrowsAny<Exception>(() => connection.ExecuteNonQuery("SELECT MyError()"));
+                Assert.ThrowsAny<Exception>(() => connection.ExecuteNonQuery("SELECT Noo1()"));
+            }
+        }
+
+        [Theory, MemberData(nameof(ConnectionData))]
+        public void ChinookDB(string extensionFile, SqliteProvider provider)
+        {
+            using (var connection = SqliteConnection.Create("Data Source=chinook.db", extensionFile, provider))
+            {
+                string commandText = "SELECT InvoiceDate FROM invoices WHERE InvoiceId = 27";
+                string result = provider switch
+                {
+                    SqliteProvider.Microsoft => connection.ExecuteScalar<string>(commandText),
+                    SqliteProvider.System => connection.ExecuteScalar<DateTime>(commandText).ToString("yyyy-MM-dd HH:mm:ss.FFF", CultureInfo.InvariantCulture),
+                    _ => throw new NotImplementedException(),
+                };
+                Assert.Equal("2009-04-22 00:00:00", result);
+                Assert.Equal("2009-04-22 00:00:00", connection.ExecuteScalar<string>("SELECT DateTimeNop(InvoiceDate) FROM invoices WHERE InvoiceId = 27"));
+            }
+        }
+
+        [Theory, MemberData(nameof(ConnectionData))]
+        public void NorthwindDB(string extensionFile, SqliteProvider provider)
+        {
+            using (var connection = SqliteConnection.Create("Data Source=northwind.db", extensionFile, provider))
+            {
+                {
+                    string commandText = "SELECT HireDate FROM Employees WHERE EmployeeId = 9";
+                    string result = provider switch
+                    {
+                        SqliteProvider.Microsoft => connection.ExecuteScalar<string>(commandText),
+                        SqliteProvider.System => connection.ExecuteScalar<DateTime>(commandText).ToString("yyyy-MM-dd HH:mm:ss.FFF", CultureInfo.InvariantCulture),
+                        _ => throw new NotImplementedException(),
+                    };
+                    string expected = provider switch
+                    {
+                        SqliteProvider.Microsoft => "2014-11-15",
+                        SqliteProvider.System => "2014-11-15 00:00:00",
+                        _ => throw new NotImplementedException(),
+                    };
+                    Assert.Equal(expected, result);
+                    Assert.Equal("2014-11-15 00:00:00", connection.ExecuteScalar<string>("SELECT DateTimeNop(HireDate) FROM Employees WHERE EmployeeId = 9"));
+                }
+                {
+                    byte[] bytes1 = connection.ExecuteScalar<byte[]>("SELECT Picture FROM Categories WHERE CategoryID = 1");
+                    Assert.Equal(10151, bytes1.Length);
+                    Assert.Equal(255, bytes1[0]);
+                    Assert.Equal(216, bytes1[1]);
+                    Assert.Equal(217, bytes1[10149]);
+                    Assert.Equal(0, bytes1[10150]);
+
+                    byte[] bytes2 = connection.ExecuteScalar<byte[]>("SELECT ShiftBlob(Picture) FROM Categories WHERE CategoryID = 1");
+                    Assert.Equal(10151, bytes2.Length);
+                    Assert.Equal(0, bytes2[0]);
+                    Assert.Equal(217, bytes2[1]);
+                    Assert.Equal(218, bytes2[10149]);
+                    Assert.Equal(1, bytes2[10150]);
+                }
+                {
+                    byte[] bytes1 = connection.ExecuteScalar<byte[]>("SELECT NullableBlob(Picture) FROM Categories WHERE CategoryID = 1");
+                    Assert.Equal(10151, bytes1.Length);
+                    Assert.Equal(255, bytes1[0]);
+
+                    Assert.Equal(DBNull.Value, connection.ExecuteScalar<DBNull>("SELECT NullableBlob(NULL)"));
+                }
             }
         }
 
