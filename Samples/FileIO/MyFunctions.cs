93db6514140816deabfc3aeab133726efc2fb8cf
Sergey Vlasov <Sergey.Vlasov@aspose.com>
2023-08-20 19:04:19 +0500

    Created FileIO sample extension.

---
+++ Samples/FileIO/MyFunctions.cs
@@ -0,0 +1,19 @@
+﻿using SqliteDna.Integration;
+
+namespace FileIO
+{
+    public static class MyFunctions
+    {
+        [SqliteTableFunction]
+        public static IEnumerable<string> ReadAllLines(string file)
+        {
+            return File.ReadAllLines(file);
+        }
+
+        [SqliteTableFunction]
+        public static IEnumerable<FileInfo> ListFiles(string path, string searchPattern)
+        {
+            return Directory.GetFiles(path, searchPattern).Select(i => new FileInfo(i));
+        }
+    }
+}
---
+++ Samples/FileIOTest/MyFunctionsTest.cs
@@ -0,0 +1,45 @@
+﻿using SqliteDna.Testing;
+
+namespace FileIOTest
+{
+    public class MyFunctionsTest
+    {
+        [Theory, MemberData(nameof(ConnectionData))]
+        public void ReadAllLines(string extensionFile, SqliteProvider provider)
+        {
+            using (var connection = SqliteConnection.Create("Data Source=:memory:", extensionFile, provider))
+            {
+                Assert.Equal(0, connection.ExecuteNonQuery("CREATE VIRTUAL TABLE FileLines USING ReadAllLines(test.txt)"));
+                using (var reader = connection.ExecuteReader("SELECT * FROM FileLines"))
+                {
+                    Assert.True(reader.Read());
+                    Assert.Equal("one", reader.GetItem<string>("Value"));
+                    Assert.True(reader.Read());
+                    Assert.Equal("two", reader.GetItem<string>("Value"));
+                    Assert.True(reader.Read());
+                    Assert.Equal("three", reader.GetItem<string>("Value"));
+                }
+            }
+        }
+
+        [Theory, MemberData(nameof(ConnectionData))]
+        public void ListFiles(string extensionFile, SqliteProvider provider)
+        {
+            using (var connection = SqliteConnection.Create("Data Source=:memory:", extensionFile, provider))
+            {
+                Assert.Equal(0, connection.ExecuteNonQuery("CREATE VIRTUAL TABLE Files USING ListFiles(., *.txt)"));
+                using (var reader = connection.ExecuteReader("SELECT * FROM Files"))
+                {
+                    Assert.True(reader.Read());
+                    Assert.Equal("test.txt", reader.GetItem<string>("Name"));
+                    Assert.Equal(15, reader.GetItem<long>("Length"));
+                    Assert.Equal(".txt", reader.GetItem<string>("Extension"));
+                }
+            }
+        }
+
+        public static IEnumerable<object[]> ConnectionData =>
+            SqliteConnection.GenerateConnectionParameters(new string[] { "FileIO" });
+    }
+
+}
